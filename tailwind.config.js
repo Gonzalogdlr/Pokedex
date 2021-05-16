5e5589a77eeeaf87b23cd723ce3c0d3fe0426166
gonzalo <gologdlr@gmail.com>
2021-05-16 02:57:33 -0500

    ADD: Images to Tailwind Background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'forest': "url('/src/assets/images/PokeForest.png')",
+        'back': "url('/src/assets/images/Back.png')",
+        'pokedex': "url('/src/assets/images/Pokedex.png')"
+      })
+    },
   },
   variants: {
     extend: {},
